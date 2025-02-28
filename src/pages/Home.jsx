c86603a5f6ac2795460f681df614f74eadf1e57a
Lorenz Held <held.lorenz.08@gmail.com>
2025-02-28 16:36:57 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+function Home() {
+
+
+    return(
+        <>
+            <h1>Lorenz Held</h1>
+            <p>Github:  <a href="https://github.com/lorenzheld" target="_blank">lorenzheld</a> </p>
+        </>
+    )
+}
+
+export default Home
