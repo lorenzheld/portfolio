ba5ef74e5c83b0020cf41828944e0f31995dc568
Lorenz Held <held.lorenz.08@gmail.com>
2025-02-28 17:43:18 +0100

    background change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ function Home() {
 
 
     return(
-        <>
+        <div className="home">
             <h1>Lorenz Held</h1>
             <p>Github:  <a href="https://github.com/lorenzheld" target="_blank">lorenzheld</a> </p>
-        </>
+        </div>
     )
 }
 
